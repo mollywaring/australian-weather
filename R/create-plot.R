4fc0d8afc6ba259fe1762212232720aad03915c6
mollywaring <molly.waring@liverpool.ac.uk>
2025-06-19 14:13:56 +0100

    Update create-plot.R
    green conflict

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour="purple")
+  geom_point(colour="green")
 
 # Save the plot in graphics/hi
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
